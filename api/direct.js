d149dbb48821cd12fa98340233d309a41bac1465
meenaishere <herokugithub663@gmail.com>
2025-12-24 16:13:31 +0530

    Create direct.js

---
+++ api/direct.js
@@ -0,0 +1,22 @@
+const { getDirectLink } = require('./_lib/terabox');
+const { sendSuccess, sendError, handleCors, validateParams } = require('./_lib/utils');
+
+module.exports = async (req, res) => {
+    if (handleCors(req, res)) return;
+    
+    try {
+        const { url, fs_id } = req.query;
+        
+        const error = validateParams(req.query, ['url', 'fs_id']);
+        if (error) {
+            return sendError(res, error, 400);
+        }
+        
+        const result = await getDirectLink(url, fs_id);
+        sendSuccess(res, result);
+        
+    } catch (error) {
+        console.error('Direct Link Error:', error.message);
+        sendError(res, error.message);
+    }
+};
